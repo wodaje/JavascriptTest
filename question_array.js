945232d82d4751ab3c296a1e24534652ea42c0e9
Jeff Woda <wodahouse@gmail.com>
2020-11-21 17:50:00 -0500

    just as in math if you hit a wall sometimes it's better to come back...

--- question_array.js
+++ question_array.js
@@ -2,7 +2,6 @@
 var questionList = [
 
     {q1: "What are the 5 main data types within Javascript?",
-
     answerChoice: {
     "A": "Boolean, skirts, functions, strings, arrays",
     "B": "Boolean, strings, objects, number, array",
@@ -10,17 +9,42 @@ var questionList = [
     "D": "Boolean, strings, arrays, functions, objects"
     },
     answerKey: "B"
-},
+    },
 
-{q2: "Can you write a function within a function?",
-
+    {q2: "Can you write a function within a function?",
     answerChoice: {
     "A": "No that is illegal and will blow up the server!",
-    "B": "You could but why would you want too?",
+    "B": "You could but would you want too?",
     "C": "Definitely yes, but only if you call it from another object!",
     "D": "Yes you can as long as you declare it as a global windows object"
     },
     answerKey: "B"
-},
+    },
+
+    {q3: "A boolean allows you to select three values!",
+    answerChoice: {
+    "A": "False",
+    "B": "True",
+    },
+    answerKey: "A"
+    },
+
+    {q4: "You don't have to worry about ending a loop as long as its written within a function",
+    answerChoice: {
+    "A": "True",
+    "B": "False",
+    },
+    answerKey: "B"
+    },
+
+    {q5: "Switch is a function within JavaScript that allows you to change the delcaration of a variable",
+    answerChoice: {
+    "A": "Most definitely, except the object to array exception",
+    "B": "No, Switch doesn't exists in JavaScript",
+    "C": "Yes, it is very useful especialy in the for loop application",
+    "D": "No, Switch is used instead of an if statement approach to select a specific path/action/approach etc"
+    },
+    answerKey: "D"
+    },
 ]
 
--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-
-// needs work
-var trueFalse  = Boolean
-var multipleChoice = 'ABCD'.split('')
-
-
 var mainEl = document.getElementById("main")
+var timeEl = document.getElementById("time")
+var questionEl = document.getElementById("question")
 
-console.log(mainEl)
+var bodyEl = document.createElement("pre")
+
 
 $("#main").on("click", function(){
 
     $("#main").text("")
+  
+bodyEl.textContent =
+
+`You will have 3 min or 180 Seconds to compete!
+
+Wrong answers will deduct 20seconds off the clock!
+
+When you are ready to test your knowlege click anywhere within this text!`
+
+mainEl.append(bodyEl)
 
 })
+
+var secondsLeft = 180
+
+function kickOff(){
+
+    mainEl.textContent = " "
+    
+    alert("This is where it all comes to and END ... no need to look further")
+    alert("No random object loop on the question array")
+    alert("No local storage exchange for final score or score display")
+    alert("No penalty application for wrong answers - hard to do since they are not even cycling lmao")
+
+
+          var timerInterval = setInterval(function() {
+          secondsLeft--;
+          console.log(secondsLeft)
+         // timeEl.textContent = "Timer: " + secondsLeft 
+      
+          //if(secondsLeft === 0) {
+          //  clearInterval(timerInterval);
+          //  sendMessage();
+          //}
+      
+        }, 1000);
+      
+
+} 
+
+
