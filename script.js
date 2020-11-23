803c3c69400efbd13fc0f2b6244d3bfcea8b05ba
Jeff Woda <wodahouse@gmail.com>
2020-11-23 18:45:17 -0500

    fixed intial text pre issue and instructions

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
+var body = document.body
+
 var mainEl = document.getElementById("main")
-var timeEl = document.getElementById("time")
 var questionEl = document.getElementById("question")
 
-var bodyEl = document.createElement("pre")
+var listEl = document.createElement("ol")
+var li1 = document.createElement("li")
+var li2 = document.createElement("li")
+var li3 = document.createElement("li")
+var li4 = document.createElement("li")
+var questionLoop = document.createElement("div")
+var timeEl = document.createElement("h1")
+var answerChoice = document.createElement("div")
+var questionList = document.createElement("div")
+
+var listItems = document.getElementsByTagName("li")
+
+var answerKey = ""
+
+//var btn = document.createElement("button")
+
+li1.textContent = "You will have 3 min or 180 Seconds to compete!"
+li2.textContent = "Wrong answers will deduct 20seconds off the clock!"
+li3.textContent = "Are you are ready to test your knowlege?"
+li4.textContent = "Click the Start button below ->Ready<- ->>Steady<<- GO!!!"
 
 
 $("#main").on("click", function(){
 
     $("#main").text("")
-  
-bodyEl.textContent =
+ 
+    mainEl.appendChild(li1);
+    listItems[0].setAttribute("style", " color:white; background: #999999; padding: 25px; margin-left: 50px;");
+    mainEl.appendChild(li2);
+    listItems[1].setAttribute("style", " color:white; background: #888888; padding: 25px; margin-left: 50px;");
+    mainEl.appendChild(li3);
+    listItems[2].setAttribute("style", " color:white; background: #777777; padding: 25px; margin-left: 50px;");
+    mainEl.appendChild(li4);  
+    listItems[3].setAttribute("style", " color:white; background: #666666; padding: 25px; margin-left: 50px;");
 
-`You will have 3 min or 180 Seconds to compete!
-
-Wrong answers will deduct 20seconds off the clock!
-
-When you are ready to test your knowlege click anywhere within this text!`
-
-mainEl.append(bodyEl)
 
 })
+//body.appendChild(btn);
 
-var secondsLeft = 180
+//listEl.setAttribute("style", "background:green; padding:200px;");
+//listEl.setAttribute("height", 300, "width", 400)
 
-function kickOff(){
+//btn1.setAttribute("height", 200)
+//btn1.setAttribute("width", 200)
 
-    mainEl.textContent = " "
+//var secondsLeft = 180
+
+//function kickOff(){
+
+//    mainEl.textContent = " "
     
-    alert("This is where it all comes to and END ... no need to look further")
-    alert("No random object loop on the question array")
-    alert("No local storage exchange for final score or score display")
-    alert("No penalty application for wrong answers - hard to do since they are not even cycling lmao")
+//    alert("This is where it all comes to and END ... no need to look further")
+ //   alert("No random object loop on the question array")
+ //   alert("No local storage exchange for final score or score display")
+ //   alert("No penalty application for wrong answers - hard to do since they are not even cycling lmao")
 
 
-          var timerInterval = setInterval(function() {
-          secondsLeft--;
-          console.log(secondsLeft)
+ //         var timerInterval = setInterval(function() {
+ //         secondsLeft--;
+ //         console.log(secondsLeft)
          // timeEl.textContent = "Timer: " + secondsLeft 
       
           //if(secondsLeft === 0) {
@@ -43,9 +70,7 @@ function kickOff(){
           //  sendMessage();
           //}
       
-        }, 1000);
+//        }, 1000);
       
 
-} 
-
-
+//} 
