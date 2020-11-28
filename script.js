12b3b6b9b799369eb5cc3d238c1c1152549f49b4
Jeff Woda <wodahouse@gmail.com>
2020-11-28 00:54:57 -0500

    cleaned up code still missing High score feature

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 var body = document.body
 
+// Set up Elments 
 var mainEl = document.getElementById("main")
 var questionEl = document.getElementById("question")
 var btnEl = document.getElementById("btn1")
+var buttonsEl = document.getElementById("buttons")
+
+// Create Elements
 var headerInstTest = document.createElement("p")
 var breakLine = document.createElement("hr")
 var breakSpace = document.createElement("br")
-var buttonsEl = document.getElementById("buttons")
-
-var questionEl = document.getElementById("question")
 var liH = document.createElement("p")
-
 var listEl = document.createElement("ol")
 var li1 = document.createElement("li")
 var li2 = document.createElement("li")
 var li3 = document.createElement("li")
 var li4 = document.createElement("li")
 var br = document.createElement("br")
-
 var timeEl = document.createElement("p")
-var answerChoiceEl = document.createElement("div")
 
+// Set Variables as well as List Element Umbrella
 var listItems = document.getElementsByTagName("li")
 var userAnswer = ""  
 var questionListEl = ""
@@ -29,44 +28,53 @@ var gameScore = 0
 var x = 0
 var y = 0
 var b = 0
+var secondsLeft = 90
 
+//Populate List for Test Start Dynamic Display
 li1.textContent = "You will have 90 Seconds to compete!"
 li2.textContent = "Wrong answers will deduct 20seconds off the clock!"
 li3.textContent = "Are you are ready to test your knowlege?"
 li4.textContent = "Click NEXT when you are ->Ready<- ->>Steady<<- GO!!!"
 
-//headerInstTest.textContent = "<<Select your answer by clicking on it directly>>"
+// Creating Buttons for Question Loop Answer choices in matching answer color closest I got this time around
+ var btnA = document.createElement("button")
+ btnA.innerHTML = "Answer: A"
+ btnA.setAttribute("style", "background-color: #999999; font-size: 20px; padding: 20px")
+ var btnB = document.createElement("button")
+ btnB.innerHTML = "Answer: B"
+ btnB.setAttribute("style", "background-color: #888888; font-size: 20px; padding: 20px")
+ var btnC = document.createElement("button")
+ btnC.innerHTML = "Answer: C"
+ btnC.setAttribute("style", "background-color: #777777; font-size: 20px; padding: 20px")
+ var btnD = document.createElement("button")
+ btnD.innerHTML = "Answer: D"
+ btnD.setAttribute("style", "background-color: #666666; font-size: 20px; padding: 20px")
 
-
+// Controls Next Button Feature with dual usage using y contorl variable
  function nextButton(){
 
     if (y === 0) {
-    mainEl.textContent = ""
+        mainEl.textContent = ""
  
-    mainEl.appendChild(li1);
-    mainEl.appendChild(li2);
-    mainEl.appendChild(li3);
-    mainEl.appendChild(li4);  
+        mainEl.appendChild(li1);
+        mainEl.appendChild(li2);
+        mainEl.appendChild(li3);
+        mainEl.appendChild(li4);  
 
-    listItems[0].setAttribute("style", " color:white; background: #999999; padding: 25px; margin-left: 50px;");
-    listItems[1].setAttribute("style", " color:white; background: #888888; padding: 25px; margin-left: 50px;");
-    listItems[2].setAttribute("style", " color:white; background: #777777; padding: 25px; margin-left: 50px;");
-    listItems[3].setAttribute("style", " color:white; background: #666666; padding: 25px; margin-left: 50px;");
+        listItems[0].setAttribute("style", " color:white; background: #999999; padding: 25px; margin-left: 50px;");
+        listItems[1].setAttribute("style", " color:white; background: #888888; padding: 25px; margin-left: 50px;");
+        listItems[2].setAttribute("style", " color:white; background: #777777; padding: 25px; margin-left: 50px;");
+        listItems[3].setAttribute("style", " color:white; background: #666666; padding: 25px; margin-left: 50px;");
 
-    y++
-
+        y++
     }
     
     else if (y === 1){
-
-       setRun()
+        setRun()
     }
-
-
 }
 
-
-
+//Starts the Test run
 function setRun(){
 
     btnEl.style.visibility = "hidden"
@@ -75,21 +83,16 @@ function setRun(){
     mainEl.removeChild(li3)
     mainEl.removeChild(li4)
 
-    
     mainEl.appendChild(timeEl)
     mainEl.appendChild(breakLine)
     mainEl.appendChild(breakSpace)
-        
-      
+         
     startTimer()
-
     initialButtons()
-
     questionLoop()
-        
 }
 
-var secondsLeft = 90
+// Sets Timer and Time out Feature
 function startTimer() {  
     timeEl.textContent = secondsLeft + " seconds to start!"
     var timerInterval = setInterval(() => {
@@ -102,35 +105,19 @@ function startTimer() {
         b++
         resetAnswer()
         initialButtons()
-            alert("Time is ER Finito... NO more Finished!")
+            //alert("Time is ER Finito... NO more Finished!")
             liH.textContent = "Your Final Score is " + gameScore
             mainEl.appendChild(liH)       
         writeFinalScore()
     }
 
-    }, 1000);
-        
-  }
+}, 1000);      
+}
 
-
-  // Creating Buttons for Question Loop Answer choices
-  var btnA = document.createElement("button")
-  btnA.innerHTML = "Answer: A"
-  btnA.setAttribute("style", "background-color: #999999; font-size: 20px; padding: 20px")
-  var btnB = document.createElement("button")
-  btnB.innerHTML = "Answer: B"
-  btnB.setAttribute("style", "background-color: #888888; font-size: 20px; padding: 20px")
-  var btnC = document.createElement("button")
-  btnC.innerHTML = "Answer: C"
-  btnC.setAttribute("style", "background-color: #777777; font-size: 20px; padding: 20px")
-  var btnD = document.createElement("button")
-  btnD.innerHTML = "Answer: D"
-  btnD.setAttribute("style", "background-color: #666666; font-size: 20px; padding: 20px")
-
+// Controls Button Display for Test Taking using b variable
 function initialButtons(){
 
-    if (b === 0){ 
-      
+    if (b === 0){   
     buttonsEl.appendChild(btnA)
     buttonsEl.appendChild(btnB)  
     buttonsEl.appendChild(btnC)  
@@ -141,17 +128,16 @@ function initialButtons(){
     btnC.addEventListener("click",function(){userAnswer ="C"; resetAnswer()})
     btnD.addEventListener("click",function(){userAnswer ="D"; resetAnswer()})  
     }
+
     else if (b ===1){
     buttonsEl.removeChild(btnA)
     buttonsEl.removeChild(btnB)  
     buttonsEl.removeChild(btnC)  
     buttonsEl.removeChild(btnD)
-
     }
-
 }
 
-
+//Accesses and loops through question obejct array
 function questionLoop(){ 
        
     userAnswer = ""
@@ -168,10 +154,10 @@ function questionLoop(){
     questionEl.appendChild(li1)
     questionEl.appendChild(li2)
     questionEl.appendChild(li3)
-    questionEl.appendChild(li4)
-      
+    questionEl.appendChild(li4)    
 }
 
+// Gathers Score and Clears Fields for next Answer set uses same end of life b variable to clean up question text
 function resetAnswer(){
 
     if(b === 0){
@@ -184,9 +170,8 @@ function resetAnswer(){
         else if(userAnswer !== answerKey){
         alert("Noooooo Wrong Answer Mon! Not <<" + userAnswer + ">> You just lost 20 Seconds!!" )
         secondsLeft = secondsLeft -20
-
         }
-    
+
         questionEl.removeChild(liH)
         questionEl.removeChild(br)
         questionEl.removeChild(li1)
@@ -196,7 +181,6 @@ function resetAnswer(){
 
         x ++
         questionLoop()
-
     } 
     
     else if (b ===1){
@@ -209,6 +193,7 @@ function resetAnswer(){
     }
 }
 
+// writes score to local storage and allows user input for high score
 function writeFinalScore(){
         alert("not User High score yet")
 
